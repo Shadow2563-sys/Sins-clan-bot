c2cfc2a8314caaaf4534402d5431a97f6e476496
Shadow2563-sys <adedejiadesida3@gmail.com>
2025-08-09 20:43:49 +0100

    Create config.js

---
+++ setting/config.js
@@ -0,0 +1,157 @@
+const fs = require('fs')
+
+global.owner = "2348093017755" //owner number
+global.footer = "𝕍ø𝕣𝕥ë𝕩_𝕊𝕙ä𝕕ø𝕨 軎" //footer section
+global.status = false //"self/public" section of the bot
+global.prefa = ['','!','.',',','🐤','🗿']
+global.owner = ['2348093017755']
+global.xprefix = '.'
+global.gambar = "https://files.catbox.moe/5ydrgs.jpeg"
+global.OWNER_NAME = "@Vortex_Shadow2563" //
+global.DEVELOPER = ["7092629860"] //
+global.BOT_NAME = "𝕆𝕓𝕝𝕚𝕧𝕚𝕠𝕟"
+global.bankowner = "Adesida Tolulope"
+global.creatorName = "𝕍ø𝕣𝕥ë𝕩_𝕊𝕙ä𝕕ø𝕨 軎"
+global.ownernumber = '2348093017755'  //creator number
+global.location = "Nigeria, Lagos, Olorunda"
+global.prefa = ['','!','.','#','&']
+//================DO NOT CHANGE OR YOU'LL GET AN ERROR=============\
+global.footer = "𝕆𝕓𝕝𝕚𝕧𝕚𝕠𝕟" //footer section
+global.link = "𝕍ø𝕣𝕥ë𝕩_𝕊𝕙ä𝕕ø𝕨 軎"
+global.autobio = true//auto update bio
+global.botName = "𝕆𝕓𝕝𝕚𝕧𝕚𝕠𝕟"
+global.version = "1.0.0"
+global.botname = "𝕆𝕓𝕝𝕚𝕧𝕚𝕠𝕟"
+global.author = "𝕍ø𝕣𝕥ë𝕩_𝕊𝕙ä𝕕ø𝕨 軎"
+global.themeemoji = '🩸'
+global.wagc = 'https://chat.whatsapp.com/Lfdqrr0BaCZ5KsxgmJiXSO'
+global.thumbnail = 'https://files.catbox.moe/5ydrgs.jpeg'
+global.packname = "𝕾𝖙𝖎𝖈𝖐𝖊𝖗 𝖇𝖞 𝖔𝖇𝖑𝖎𝖛𝖎𝖔𝖓𝒆"
+global.author = "\n\n\n\n\nCreated by Lord Shadow\nMy master telegram username: @Vortex_Shadow2563𝟛"
+global.creator = "2348093017755@s.whatsapp.net"
+global.ownername = '"𝕍ø𝕣𝕥ë𝕩_𝕊𝕙ä𝕕ø𝕨 軎' 
+global.onlyowner = `⚠️ 𝘚𝘰𝘳𝘳𝘺, 𝘰𝘯𝘭𝘺 𝘧𝘰𝘳 𝘖𝘞𝘕𝘌𝘙𝘚 ⚠️
+➤ 𝘊𝘰𝘯𝘵𝘢𝘤𝘵: @Vortex_Shadow2563
+➤ 𝘛𝘰 𝘣𝘦 𝘢𝘯 𝘰𝘸𝘯𝘦𝘳`
+
+global.database = `📂 𝘛𝘰 𝘣𝘦 𝘪𝘯 𝘵𝘩𝘦 𝘋𝘈𝘛𝘈𝘉𝘈𝘚𝘌
+➤ 𝘊𝘰𝘯𝘵𝘢𝘤𝘵: @Vortex_Shadow2563*`
+
+global.mess = {
+    wait: "Hold.",
+    success: "✅ 𝑺𝒖𝒄𝒄𝒆𝒔𝒔",
+    on: "🟢 𝕆𝕓𝕝𝕚𝕧𝕚𝕠𝕟 is now On",
+    prem: "⚠️ FOR PREMIUM USERS ONLY ⚠️\n➤ Add your number to the DATABASE to access premium features",
+    off: "🔴 𝕆𝕓𝕝𝕚𝕧𝕚𝕠𝕟 is now 𝘖𝘍𝘍",
+    query: {
+        text: "❓ 𝘞𝘩𝘦𝘳𝘦'𝘴 𝘵𝘩𝘦 𝘵𝘦𝘹𝘵, 𝘮𝘢𝘯?",
+        link: "❓ 𝘞𝘩𝘦𝘳𝘦'𝘴 𝘵𝘩𝘦 𝘭𝘪𝘯𝘬, 𝘣𝘳𝘰?"
+    },
+    error: {
+        fitur: "⚠️ 𝘚𝘰𝘳𝘳𝘺 𝘣𝘳𝘰, 𝘵𝘩𝘦 𝘧𝘦𝘢𝘵𝘶𝘳𝘦 𝘩𝘢𝘴 𝘦𝘳𝘳𝘰𝘳. 𝘊𝘩𝘢𝘵 𝘣𝘰𝘵 𝘥𝘦𝘷 𝘵𝘰 𝘧𝘪𝘹 𝘪𝘵."
+    },
+    only: {
+        group: "❌ 𝘛𝘩𝘪𝘴 𝘧𝘦𝘢𝘵𝘶𝘳𝘦 𝘤𝘢𝘯 𝘰𝘯𝘭𝘺 𝘣𝘦 𝘶𝘴𝘦𝘥 𝘪𝘯 𝘎𝘙𝘖𝘜𝘗𝘚",
+        private: "❌ 𝘛𝘩𝘪𝘴 𝘧𝘦𝘢𝘵𝘶𝘳𝘦 𝘤𝘢𝘯 𝘰𝘯𝘭𝘺 𝘣𝘦 𝘶𝘴𝘦𝘥 𝘪𝘯 𝘗𝘙𝘐𝘝𝘈𝘛𝘌 𝘊𝘏𝘈𝘛𝘚",
+        owner: "❌ 𝘖𝘕𝘓𝘠 𝘚𝘏𝘈𝘋𝘖𝘞 𝘊𝘈𝘕 𝘜𝘚𝘌 𝘛𝘏𝘐𝘚",
+        admin: "❌ 𝘖𝘕𝘓𝘠  𝘈𝘋𝘔𝘐𝘕𝘚 𝘊𝘈𝘕 𝘜𝘚𝘌 𝘛𝘏𝘐𝘚",
+        badmin: "⚠️ 𝘠𝘰𝘶 𝘤𝘢𝘯'𝘵 𝘶𝘴𝘦 𝘵𝘩𝘪𝘴 𝘣𝘦𝘤𝘢𝘶𝘴𝘦 𝘣𝘰𝘵 𝘪𝘴 𝘯𝘰𝘵 𝘢𝘯 𝘢𝘥𝘮𝘪𝘯",
+        premium: "💎 𝘗𝘳𝘦𝘮𝘪𝘶𝘮 𝘧𝘦𝘢𝘵𝘶𝘳𝘦 - 𝘰𝘯𝘭𝘺 𝘧𝘰𝘳 𝘱𝘳𝘦𝘮𝘪𝘶𝘮 𝘶𝘴𝘦𝘳𝘴"
+    }
+}
+
+global.hituet = 0
+//false=disable and true=enable
+global.autoRecording = true //auto recording
+global.autoTyping = true //auto typing
+global.autorecordtype = true //auto typing + recording
+global.autoread = false //auto read messages
+global.autobio = true //auto update bio
+global.anti92 = true //auto block +92 
+global.autoswview = true //auto view status/story
+
+let file = require.resolve(__filename)
+require('fs').watchFile(file, () => {
+  require('fs').unwatchFile(file)
+  console.log('\x1b[0;32m'+__filename+' \x1b[1;32mupdated!\x1b[0m')
+  delete require.cache[file]
+  require(file)
+}) const fs = require('fs')
+
+global.owner = "2348093017755" //owner number
+global.footer = "𝕍ø𝕣𝕥ë𝕩_𝕊𝕙ä𝕕ø𝕨 軎" //footer section
+global.status = false //"self/public" section of the bot
+global.prefa = ['','!','.',',','🐤','🗿']
+global.owner = ['2348093017755']
+global.xprefix = '.'
+global.gambar = "https://files.catbox.moe/5ydrgs.jpeg"
+global.OWNER_NAME = "@Vortex_Shadow2563" //
+global.DEVELOPER = ["7092629860"] //
+global.BOT_NAME = "𝕆𝕓𝕝𝕚𝕧𝕚𝕠𝕟"
+global.bankowner = "Adesida Tolulope"
+global.creatorName = "𝕍ø𝕣𝕥ë𝕩_𝕊𝕙ä𝕕ø𝕨 軎"
+global.ownernumber = '2348093017755'  //creator number
+global.location = "Nigeria, Lagos, Olorunda"
+global.prefa = ['','!','.','#','&']
+//================DO NOT CHANGE OR YOU'LL GET AN ERROR=============\
+global.footer = "𝕆𝕓𝕝𝕚𝕧𝕚𝕠𝕟" //footer section
+global.link = "𝕍ø𝕣𝕥ë𝕩_𝕊𝕙ä𝕕ø𝕨 軎"
+global.autobio = true//auto update bio
+global.botName = "𝕆𝕓𝕝𝕚𝕧𝕚𝕠𝕟"
+global.version = "1.0.0"
+global.botname = "𝕆𝕓𝕝𝕚𝕧𝕚𝕠𝕟"
+global.author = "𝕍ø𝕣𝕥ë𝕩_𝕊𝕙ä𝕕ø𝕨 軎"
+global.themeemoji = '🩸'
+global.wagc = 'https://chat.whatsapp.com/Lfdqrr0BaCZ5KsxgmJiXSO'
+global.thumbnail = 'https://files.catbox.moe/5ydrgs.jpeg'
+global.packname = "𝕾𝖙𝖎𝖈𝖐𝖊𝖗 𝖇𝖞 𝖔𝖇𝖑𝖎𝖛𝖎𝖔𝖓𝒆"
+global.author = "\n\n\n\n\nCreated by Lord Shadow\nMy master telegram username: @Vortex_Shadow2563𝟛"
+global.creator = "2348093017755@s.whatsapp.net"
+global.ownername = '"𝕍ø𝕣𝕥ë𝕩_𝕊𝕙ä𝕕ø𝕨 軎' 
+global.onlyowner = `⚠️ 𝘚𝘰𝘳𝘳𝘺, 𝘰𝘯𝘭𝘺 𝘧𝘰𝘳 𝘖𝘞𝘕𝘌𝘙𝘚 ⚠️
+➤ 𝘊𝘰𝘯𝘵𝘢𝘤𝘵: @Vortex_Shadow2563
+➤ 𝘛𝘰 𝘣𝘦 𝘢𝘯 𝘰𝘸𝘯𝘦𝘳`
+
+global.database = `📂 𝘛𝘰 𝘣𝘦 𝘪𝘯 𝘵𝘩𝘦 𝘋𝘈𝘛𝘈𝘉𝘈𝘚𝘌
+➤ 𝘊𝘰𝘯𝘵𝘢𝘤𝘵: @Vortex_Shadow2563*`
+
+global.mess = {
+    wait: "Hold.",
+    success: "✅ 𝑺𝒖𝒄𝒄𝒆𝒔𝒔",
+    on: "🟢 𝕆𝕓𝕝𝕚𝕧𝕚𝕠𝕟 is now On",
+    prem: "⚠️ FOR PREMIUM USERS ONLY ⚠️\n➤ Add your number to the DATABASE to access premium features",
+    off: "🔴 𝕆𝕓𝕝𝕚𝕧𝕚𝕠𝕟 is now 𝘖𝘍𝘍",
+    query: {
+        text: "❓ 𝘞𝘩𝘦𝘳𝘦'𝘴 𝘵𝘩𝘦 𝘵𝘦𝘹𝘵, 𝘮𝘢𝘯?",
+        link: "❓ 𝘞𝘩𝘦𝘳𝘦'𝘴 𝘵𝘩𝘦 𝘭𝘪𝘯𝘬, 𝘣𝘳𝘰?"
+    },
+    error: {
+        fitur: "⚠️ 𝘚𝘰𝘳𝘳𝘺 𝘣𝘳𝘰, 𝘵𝘩𝘦 𝘧𝘦𝘢𝘵𝘶𝘳𝘦 𝘩𝘢𝘴 𝘦𝘳𝘳𝘰𝘳. 𝘊𝘩𝘢𝘵 𝘣𝘰𝘵 𝘥𝘦𝘷 𝘵𝘰 𝘧𝘪𝘹 𝘪𝘵."
+    },
+    only: {
+        group: "❌ 𝘛𝘩𝘪𝘴 𝘧𝘦𝘢𝘵𝘶𝘳𝘦 𝘤𝘢𝘯 𝘰𝘯𝘭𝘺 𝘣𝘦 𝘶𝘴𝘦𝘥 𝘪𝘯 𝘎𝘙𝘖𝘜𝘗𝘚",
+        private: "❌ 𝘛𝘩𝘪𝘴 𝘧𝘦𝘢𝘵𝘶𝘳𝘦 𝘤𝘢𝘯 𝘰𝘯𝘭𝘺 𝘣𝘦 𝘶𝘴𝘦𝘥 𝘪𝘯 𝘗𝘙𝘐𝘝𝘈𝘛𝘌 𝘊𝘏𝘈𝘛𝘚",
+        owner: "❌ 𝘖𝘕𝘓𝘠 𝘚𝘏𝘈𝘋𝘖𝘞 𝘊𝘈𝘕 𝘜𝘚𝘌 𝘛𝘏𝘐𝘚",
+        admin: "❌ 𝘖𝘕𝘓𝘠  𝘈𝘋𝘔𝘐𝘕𝘚 𝘊𝘈𝘕 𝘜𝘚𝘌 𝘛𝘏𝘐𝘚",
+        badmin: "⚠️ 𝘠𝘰𝘶 𝘤𝘢𝘯'𝘵 𝘶𝘴𝘦 𝘵𝘩𝘪𝘴 𝘣𝘦𝘤𝘢𝘶𝘴𝘦 𝘣𝘰𝘵 𝘪𝘴 𝘯𝘰𝘵 𝘢𝘯 𝘢𝘥𝘮𝘪𝘯",
+        premium: "💎 𝘗𝘳𝘦𝘮𝘪𝘶𝘮 𝘧𝘦𝘢𝘵𝘶𝘳𝘦 - 𝘰𝘯𝘭𝘺 𝘧𝘰𝘳 𝘱𝘳𝘦𝘮𝘪𝘶𝘮 𝘶𝘴𝘦𝘳𝘴"
+    }
+}
+
+global.hituet = 0
+//false=disable and true=enable
+global.autoRecording = true //auto recording
+global.autoTyping = true //auto typing
+global.autorecordtype = true //auto typing + recording
+global.autoread = false //auto read messages
+global.autobio = true //auto update bio
+global.anti92 = true //auto block +92 
+global.autoswview = true //auto view status/story
+
+let file = require.resolve(__filename)
+require('fs').watchFile(file, () => {
+  require('fs').unwatchFile(file)
+  console.log('\x1b[0;32m'+__filename+' \x1b[1;32mupdated!\x1b[0m')
+  delete require.cache[file]
+  require(file)
+}) 
